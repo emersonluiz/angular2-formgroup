1a88d63876f5358ec8b246a144ce91588c8071c4
Emerson Luiz de Castro <ecastro@controlunion.com.br>
2016-11-10 22:37:17 -0200

    add examples

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,27 +1,54 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: 'app/app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   first: string
   last: string
 
-  personForm: FormGroup
+  name: string
+  legalName: string
 
-  constructor() {
+  search: string
+
+  personForm: FormGroup
+  organizationForm: FormGroup
+  searchForm: FormGroup
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
     this.personForm = new FormGroup({
       firstName: new FormControl(),
       lastName: new FormControl()
     })
+
+    this.organizationForm = this.formBuilder.group({
+      name:['', Validators.required],
+      legalName: ''
+    })
+
+    this.searchForm = new FormGroup({
+      search: new FormControl()
+    })
+
+    this.searchForm.controls["search"].valueChanges.subscribe(value => {
+      this.search = value;
+    })
   }
 
-  onSave() {
+  onSavePerson() {
     this.first = this.personForm.value.firstName
     this.last = this.personForm.value.lastName
+  }
+
+  onSaveOrganization() {
+    this.name = this.organizationForm.value.name
+    this.legalName = this.organizationForm.value.legalName
   }
 
 }
